5d6ddfb218530fd7e15767f79d0dd41c759bf2db
Aymeric Barantal <mric@gandi.net>
2014-12-23 12:54:37 +0100

    rework common.sh for better integration

--- common.sh
+++ common.sh
@@ -1,8 +1,28 @@
-# Default values
+#!/usr/bin/env bash
+
+# Load local variables
+if [ -f caliopen.env ]; then
+    echo "Loading env from caliopen.env"
+    source caliopen.env
+fi
+
+# All backends components
+CALIOPEN_COMPONENTS="config storage core api messaging smtp web cli"
+
+# Default values that can be overidden in a cassandra.env file
 DEFAULT_BASE_URL="https://github.com/CaliOpen"
-DEFAULT_LOCAL_DIR="../code"
-PROJECT_NAME="caliopen"
+DEFAULT_LOCAL_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )/.." && pwd )"
 
-COMPONENTS="config storage core api messaging smtp ng web"
 
-export DEFAULT_BASE_URL DEFAULT_LOCAL_DIR PROJECT_NAME COMPONENTS
+# Set BASE_URL LOCAL_DIR
+if [[ "x${BASE_URL}" == "x" ]]; then
+    BASE_URL="${DEFAULT_BASE_URL}"
+fi
+
+if [[ "x${LOCAL_DIR}" == "x" ]]; then
+    LOCAL_DIR="${DEFAULT_LOCAL_DIR}"
+fi
+
+CALIOPEN_DIR="${LOCAL_DIR}"
+
+export BASE_URL CALIOPEN_DIR CALIOPEN_COMPONENTS

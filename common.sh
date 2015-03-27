d508693e388dd98993ca36c67d9a51aa9cb24cb9
Aymeric Barantal <mric@gandi.net>
2015-03-27 18:01:21 +0100

    Rename caliopen components to new structure
    Signed-off-by: Aymeric Barantal <mric@gandi.net>

--- common.sh
+++ common.sh
@@ -7,7 +7,7 @@ if [ -f caliopen.env ]; then
 fi
 
 # All backends components
-CALIOPEN_COMPONENTS="config storage core api messaging smtp web cli frontend"
+CALIOPEN_COMPONENTS="base base.message api.base messaging smtp web cli frontend"
 
 # Default values that can be overidden in a cassandra.env file
 DEFAULT_BASE_URL="https://github.com/CaliOpen"

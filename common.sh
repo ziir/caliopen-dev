46b431b70b8d4f224d04ec253f9710ac974caef7
Julien Muetton <julien@muetton.me>
2015-05-26 12:16:31 +0200

    Update default caliopen git base url

--- common.sh
+++ common.sh
@@ -10,7 +10,7 @@ fi
 CALIOPEN_COMPONENTS="base base.user base.message api.base api.user api.message messaging smtp web cli frontend"
 
 # Default values that can be overidden in a cassandra.env file
-DEFAULT_BASE_URL="git@github.com:CaliOpen"
+DEFAULT_BASE_URL="https://github.com/CaliOpen"
 DEFAULT_LOCAL_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )/.." && pwd )"
 
 
